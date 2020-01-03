287c1d7465ac78dfd68a5b2a42201ab8a87c87b7
hans <leekorea09@gmail.com>
2020-01-04 02:55:01 +0900

    init

---
+++ content/__about/resume-en.md
@@ -0,0 +1,71 @@
+---
+title: 'about'
+date: 2019-1-27 16:21:13
+lang: 'en'
+---
+
+## about me
+i'm an agile web developer. enjoy playing games, investments, movies, camping, cooking, playmobil, books. 
+i try to embrace various experience to become better developer. 
+i try to keep my personal standards(learn, apply, share) whether i making a simple web or a massive multi-purpose web applicaiton.
+
+## currently interested in..
+-- SEO
+
+-- 🐳 Docker
+
+-- Platform business
+
+-- AWS
+
+-- Tesla 
+
+## skills
+-- Javascript
+
+-- CSS3
+
+-- HTML5
+
+-- SCSS
+
+-- Ruby
+
+-- Node
+
+
+## frameworks
+-- React
+
+-- Angular
+
+-- Koa
+
+-- Ruby on Rails
+
+
+## build tools
+-- Babel
+
+-- Webpack
+
+
+## management
+-- Jira
+
+-- Git
+
+-- Wiki
+
+-- 🐳 Docker
+
+-- Jenkins
+
+
+## history
+-- Kakao Commerce
+
+-- Kakao
+
+-- VINYL X
+
---
+++ gatsby-meta-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: `hans.page`,
+  description: `🙊 오다주었어... 🧨`,
+  author: `hans`,
+  introduction: `☀️에는 한스, 🌙에는 이작가야`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: `https://twitter.com/hans__hk`, // Your Twitter account
+    github: `https://github.com/hans-hk`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: `https://facebook.com/leekorea09`, // Your Facebook account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'Ka30PDz',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
